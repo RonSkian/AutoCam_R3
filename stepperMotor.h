0292caab8201b07d77c5beef6d5bb3b7c25a23f6
RonSkian <kwan.ronnie@gmail.com>
2015-04-07 13:13:10 -0700

    general comment change

--- stepperMotor.h
+++ stepperMotor.h
@@ -3,6 +3,10 @@
 
 void updt_stepperInfo(void);
 void step(unsigned char dir, unsigned char stepMS, unsigned char stepLS);
+// char dir = Direction (TRUE = CW = towards motor, FALSE = CCW = away from motor; note orientation is with the bottom of motor facing you)
+// char stepMS = Most significant step value
+// char stepLS = Least significant step value
+// e.g 1.5 mm, stepMS = 1, stepLS = 5
 void performStepping(void);
 void reverseSteps(void);
 void homeCarriage(unsigned char dir);
