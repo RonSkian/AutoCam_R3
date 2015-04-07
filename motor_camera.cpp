d239ea8c47c9cc69539a34779bb45387261d52a8
RonSkian <kwan.ronnie@gmail.com>
2015-04-07 12:22:01 -0700

    1.01 Start to synergize motor and camera logic

---
+++ motor_camera.cpp
@@ -0,0 +1,9 @@
+#include "motor_camera.h"
+#include "stepperMotor.h"
+#include "cameraShutter.h"
+#include "globalVariables.h"
+#include "misc.h"
+
+void mCamContinuous(unsigned char NOF, unsigned char SSC, unsigned char TBF){
+
+}
---
+++ motor_camera.h
@@ -0,0 +1,10 @@
+#ifndef MOTOR_CAMERA_H_
+#define MOTOR_CAMERA_H_
+
+void mCamContinuous(unsigned char NOF, unsigned char SSC, unsigned char TBF);
+	// This function allows the camera to continously shoot given the parameters set by the user
+	// NOF = Number of Frames
+	// SSC = Shutter Speed Compensation (measured in seconds)
+	// TBF  = Time Between Frames
+
+#endif
