f233012e1c7659cfd5c7fca100a89fe250ef4ca1
RonSkian <kwan.ronnie@gmail.com>
2015-04-07 13:26:45 -0700

    motor camera workflow barely done

--- motor_camera.cpp
+++ motor_camera.cpp
@@ -1,10 +1,17 @@
+#include <avr/io.h>
+#include <Arduino.h>
+#include <avr/interrupt.h>
 #include "motor_camera.h"
 #include "stepperMotor.h"
 #include "cameraShutter.h"
 #include "globalVariables.h"
 #include "misc.h"
 
-void mCamContinuous(unsigned char NOF, unsigned char SSC, unsigned char TBF){
-
-
+void mCamContinuous(unsigned char NOFP, unsigned char SSC, unsigned char TBF, unsigned char NOFT, unsigned char DBF, unsigned char dir, unsigned char stepMS, unsigned char stepLS){
+	unsigned char cNOFT;
+	while(cNOFT != NOFT){
+		camContinuous(NOFP, SSC, TBF);
+		step(dir, stepMS, stepLS);
+		delay(10);
+	}
 }
--- motor_camera.h
+++ motor_camera.h
@@ -1,11 +1,16 @@
 #ifndef MOTOR_CAMERA_H_
 #define MOTOR_CAMERA_H_
 
-void mCamContinuous(unsigned char NOF, unsigned char SSC, unsigned char TBF);
+void mCamContinuous(unsigned char NOFP, unsigned char SSC, unsigned char TBF, unsigned char NOFT, unsigned char DBF, unsigned char dir, unsigned char stepMS, unsigned char stepLS);
 	// This function allows the camera to continously shoot given the parameters set by the user
-	// NOF = Number of Frames
+	// NOFP = Number of Frames Per Position
 	// SSC = Shutter Speed Compensation (measured in seconds)
-	// TBF  = Time Between Frames
-
+	// TBF = Time Between Frames
+	// NOFT = Number of Frames Total
+	// DBF = Distance Between Frames 
+	// dir = Direction (TRUE = CW = towards motor, FALSE = CCW = away from motor; note orientation is with the bottom of motor facing you)
+	// stepMS = Most significant step value
+	// stepLS = Least significant step value
+	//		e.g 1.5 mm, stepMS = 1, stepLS = 5
 
 #endif
