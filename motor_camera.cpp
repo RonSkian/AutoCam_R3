263bda8088d63fd2b0e3ecbbc5a6f341610cfad5
RonSkian <kwan.ronnie@gmail.com>
2015-04-08 13:33:20 -0700

    updated the workflow, should work properly, requires extra delay for shutter

--- motor_camera.cpp
+++ motor_camera.cpp
@@ -7,11 +7,30 @@
 #include "globalVariables.h"
 #include "misc.h"
 
-void mCamContinuous(unsigned char NOFP, unsigned char SSC, unsigned char TBF, unsigned char NOFT, unsigned char DBF, unsigned char dir, unsigned char stepMS, unsigned char stepLS){
+#define moduloDivider 10
+
+void mCamContinuous(unsigned char NOFP, unsigned char SSC, unsigned char TBF, unsigned char NOFT, unsigned char DBF, unsigned char dir){
+	// This function allows the camera to continously shoot given the parameters set by the user
+	// NOFP = Number of Frames Per Position
+	// SSC = Shutter Speed Compensation (measured in seconds)
+	// TBF = Time Between Frames
+	// NOFT = Number of Frames Total
+ 	// DBF = Distance Between Frames (1mm increments only)
+	// dir = Direction (TRUE = CW = towards motor, FALSE = CCW = away from motor; note orientation is with the bottom of motor facing you)
+	
 	unsigned char cNOFT;
-	while(cNOFT != NOFT){
+	unsigned char sQ = DBF/moduloDivider;
+	//DBF quotient for step function
+	unsigned char sR = DBF%moduloDivider;
+	//DBF remainder for step function
+	while(NOFT != cNOFT){
 		camContinuous(NOFP, SSC, TBF);
-		step(dir, stepMS, stepLS);
 		delay(10);
+		//Minor delay stop any vibrations from affecting the shot prior to movement
+		step(dir, sQ, sR);
+		disableMotor();
+		Serial.print("moved: ");
+		Serial.println(DBF);
+		delay(1000);
 	}
 }
