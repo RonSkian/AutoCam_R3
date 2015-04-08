9f90ccf6a2b5f99adaf3de0a0f129b3a99cb5e40
RonSkian <kwan.ronnie@gmail.com>
2015-04-08 13:34:48 -0700

    updated the mCamContinuous function scope, removed variables

--- motor_camera.h
+++ motor_camera.h
@@ -1,16 +1,13 @@
 #ifndef MOTOR_CAMERA_H_
 #define MOTOR_CAMERA_H_
 
-void mCamContinuous(unsigned char NOFP, unsigned char SSC, unsigned char TBF, unsigned char NOFT, unsigned char DBF, unsigned char dir, unsigned char stepMS, unsigned char stepLS);
+void mCamContinuous(unsigned char NOFP, unsigned char SSC, unsigned char TBF, unsigned char NOFT, unsigned char DBF, unsigned char dir);
 	// This function allows the camera to continously shoot given the parameters set by the user
 	// NOFP = Number of Frames Per Position
 	// SSC = Shutter Speed Compensation (measured in seconds)
 	// TBF = Time Between Frames
 	// NOFT = Number of Frames Total
-	// DBF = Distance Between Frames 
+	// DBF = Distance Between Frames (1mm increments only)
 	// dir = Direction (TRUE = CW = towards motor, FALSE = CCW = away from motor; note orientation is with the bottom of motor facing you)
-	// stepMS = Most significant step value
-	// stepLS = Least significant step value
-	//		e.g 1.5 mm, stepMS = 1, stepLS = 5
 
 #endif
